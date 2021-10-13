737cc136f7b38a2a9d1124df498f4ccc6dadeab0
Luke Phyall <shoreditchwarlock@gmail.com>
2021-10-13 12:52:48 +0100

    Adds basic check for ethereum object on window

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+interface WindowWithEtherum extends Window {
+  ethereum: any;
+}
+
+const isWindowWithEtherum = (window: Window): window is WindowWithEtherum => {
+  return (window as WindowWithEtherum)?.ethereum;
+};
 
 function App() {
+  const checkWalletConnection = (): void => {
+    if (isWindowWithEtherum(window)) {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Please ensure Metamask is connected');
+      } else {
+        console.log('Ethereum object: ', ethereum);
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
   return (
     <div>
       <h1>Solidity Wave Portal</h1>
